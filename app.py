3c03580a58f817278150557ada10c07a41bd7f78
edyta <jozdowskaedyta@wp.pl>
2020-04-15 07:26:25 +0200

    initial

---
+++ app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# flask/app.py
+
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for, flash
+from jinja2 import Template
+from lookAndSay import lineSeq
+
+app = Flask(__name__)
+# konfiguracja aplikacji
+app.config.update(dict(
+    SECRET_KEY='bradzosekretnawartosc',
+))
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        odpowiedzi = request.form.to_dict()
+        
+        if not odpowiedzi["number"] or not odpowiedzi["iterations"]:
+            flash("Wypełnij brakujące pola",'error')
+        else:
+            result = lineSeq(odpowiedzi["number"],int(odpowiedzi["iterations"]))
+            responseStr=""
+            for i in range(len(result)):
+                responseStr +=  "<li>#%s: %s</li>" % (i+1, result[i])
+                flash(responseStr,'result')
+                flash(odpowiedzi["number"], 'number')
+                flash(odpowiedzi["iterations"], 'iterations')
+        return redirect(url_for('index') )
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ lookAndSay.py
@@ -0,0 +1,25 @@
+def lookAndSay(number, r):
+  res=[]
+  chars = (number+" ").split(" ")
+  count = 0
+  prev = chars[0]
+
+  for i in range(len(chars)):
+    ne=chars[i]
+    if (ne==prev):
+      count += 1
+    else:
+      res.append(str(count))
+      res.append(prev)
+      prev = ne
+      count = 1
+  return " ".join(res)
+
+def lineSeq(nu, line):
+  res = nu
+  All = []
+  for i in range(0, line):
+    All.append(res)
+    res = lookAndSay(res,nu)
+  return All
+
