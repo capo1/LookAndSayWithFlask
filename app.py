86be480e22d6c977e65d81270b73b31766f8aee4
edyta <jozdowskaedyta@wp.pl>
2020-04-15 09:41:27 +0200

    minor changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # Konfiguracja aplikacji, w sumie nie wiem póki co, po co to jest,
 # było w przykładzie z jakiego korzystałam :shrug:
 app.config.update(dict(
-    SECRET_KEY='bradzosekretnawartosc',
+    SECRET_KEY="bradzosekretnawartosc",
 ))
 
 title = "Look and Say - Python Flask Example" 
@@ -19,29 +19,29 @@ title = "Look and Say - Python Flask Example"
 # główny route aplikacji, obsługujący dwie metody GET i POST
 # GET - potrzebny do wyświetlenia
 # POST - potrzebny do wysłania danych z formularza
-@app.route('/', methods = ['GET', 'POST'])
+@app.route("/", methods = ["GET", "POST"])
 
 # Funkcja obsługująca wysłanie formularza, bardzo uproszczona
 def index():
     # Określenie, że żądanie pochodzi z POST'a
-    if request.method == 'POST':
+    if request.method == "POST":
         
         # przypisanie zmiennych z pól formularza do dict
         odpowiedzi = request.form.to_dict()
         
         # podstawowa walidacja formularza
         if not odpowiedzi["number"] or not odpowiedzi["iterations"]:
-            flash("Please, fill required fields", 'error')
+            flash("Please, fill required fields", "error")
         else:
             # Wykonanie LookAndSay
             results = lineSeq(odpowiedzi["number"], int(odpowiedzi["iterations"]))
             
         #przekierowanie do odpowiedniego url'a
-        return render_template('index.html', title = title, results = results, inputs = {"number": odpowiedzi["number"], 'iterations': odpowiedzi["iterations"]})
+        return render_template("index.html", title = title, results = results, inputs = {"number": odpowiedzi["number"], "iterations": odpowiedzi["iterations"]})
     
     # wyrenderowanie części templeta, 
     # przekazanie zdefiniowanej zmiennej, tak dla przykładu :)
-    return render_template('index.html', title = title, inputs = {"number": 5, 'iterations': 10} )
+    return render_template("index.html", title = title, inputs = {"number": 5, "iterations": 10} )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug = True)
